75aa7a7b97cb95c6eaf41357b00d1a3e4d0da769
zernitta <zernitta@sina.cn>
2016-10-12 13:12:28 +0800

    first sample

---
+++ imports/ui/components/partiesList/partiesList.js
@@ -0,0 +1,29 @@
+import angular from 'angular';
+import angularMeteor from 'angular-meteor';
+
+import templateUrl from './partiesList.html';
+
+class PartiesList {
+    constructor($scope, $reactive) {
+        'ngInject';
+
+        $reactive(this).attach($scope);
+
+        this.helpers({
+            parties() {
+                return Parties.find({});
+            }
+        });
+    }
+}
+
+const name = 'partiesList';
+
+// create a module
+export default angular.module(name, [
+    angularMeteor
+]).component(name, {
+    templateUrl: templateUrl,
+    controllerAs: name,
+    controller: PartiesList
+});
